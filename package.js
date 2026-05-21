c958a00fd95c259652577ea14fd0ee0ca4a31316
Nitin Kumar <nitin.kumar@spyne.ai>
2026-05-21 16:42:49 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "qc-dashboard-slack-bot",
+  "version": "1.0.0",
+  "description": "Hourly screenshot of QC Pendency Dashboard → Slack",
+  "main": "screenshot-bot.js",
+  "scripts": {
+    "start": "node screenshot-bot.js"
+  },
+  "dependencies": {
+    "puppeteer": "^21.0.0"
+  }
+}
